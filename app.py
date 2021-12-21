1b9e66e15705d84e800d00db65228f2b2d6a2ced
AnHell999 <angel.vieitez@hotmail.com>
2021-12-21 17:13:51 +0100

    Plantilla del negro

--- app.py
+++ app.py
@@ -1,121 +1,202 @@
-import streamlit as st
-import pandas as pd
-import numpy as np
-import requests
-import base64
+# DB
+import sqlite3
+conn = sqlite3.connect('data.db')
+c = conn.cursor()
+
+# Functions
+def create_table():
+	c.execute('CREATE TABLE IF NOT EXISTS blogtable(author TEXT,title TEXT,article TEXT,postdate DATE)')
+
+def add_data(author,title,article,postdate):
+	c.execute('INSERT INTO blogtable(author,title,article,postdate) VALUES (?,?,?,?)',(author,title,article,postdate))
+	conn.commit()
+
+def view_all_notes():
+	c.execute('SELECT * FROM blogtable')
+	data = c.fetchall()
+	return data
+
+def view_all_titles():
+	c.execute('SELECT DISTINCT title FROM blogtable')
+	data = c.fetchall()
+	return data
 
 
-def _max_width_():
-    max_width_str = f"max-width: 1100px;"
-    st.markdown(
-        f"""
-    <style>
-    .reportview-container .main .block-container{{
-        {max_width_str}
-    }}
-    </style>    
-    """,
-        unsafe_allow_html=True, 
-    )
+def get_blog_by_title(title):
+	c.execute('SELECT * FROM blogtable WHERE title="{}"'.format(title))
+	data = c.fetchall()
+	return data
+def get_blog_by_author(author):
+	c.execute('SELECT * FROM blogtable WHERE author="{}"'.format(author))
+	data = c.fetchall()
+	return data
 
-_max_width_()
+def delete_data(title):
+	c.execute('DELETE FROM blogtable WHERE title="{}"'.format(title))
+	conn.commit()
 
 
-st.title("🌐 HTML Table Scraper 🕸️")
-st.markdown(" A simple HTML table scraper made in Python 🐍 & the amazing [Streamlit!](https://www.streamlit.io/) ")
+# Layout Templates
+html_temp = """
+<div style="background-color:{};padding:10px;border-radius:10px">
+<h1 style="color:{};text-align:center;">Simple Blog </h1>
+</div>
+"""
+title_temp ="""
+<div style="background-color:#464e5f;padding:10px;border-radius:10px;margin:10px;">
+<h4 style="color:white;text-align:center;">{}</h1>
+<img src="https://www.w3schools.com/howto/img_avatar.png" alt="Avatar" style="vertical-align: middle;float:left;width: 50px;height: 50px;border-radius: 50%;" >
+<h6>Author:{}</h6>
+<br/>
+<br/> 
+<p style="text-align:justify">{}</p>
+</div>
+"""
+article_temp ="""
+<div style="background-color:#464e5f;padding:10px;border-radius:5px;margin:10px;">
+<h4 style="color:white;text-align:center;">{}</h1>
+<h6>Author:{}</h6> 
+<h6>Post Date: {}</h6>
+<img src="https://www.w3schools.com/howto/img_avatar.png" alt="Avatar" style="vertical-align: middle;width: 50px;height: 50px;border-radius: 50%;" >
+<br/>
+<br/>
+<p style="text-align:justify">{}</p>
+</div>
+"""
+head_message_temp ="""
+<div style="background-color:#464e5f;padding:10px;border-radius:5px;margin:10px;">
+<h4 style="color:white;text-align:center;">{}</h1>
+<img src="https://www.w3schools.com/howto/img_avatar.png" alt="Avatar" style="vertical-align: middle;float:left;width: 50px;height: 50px;border-radius: 50%;">
+<h6>Author:{}</h6> 
+<h6>Post Date: {}</h6> 
+</div>
+"""
+full_message_temp ="""
+<div style="background-color:silver;overflow-x: auto; padding:10px;border-radius:5px;margin:10px;">
+<p style="text-align:justify;color:black;padding:10px">{}</p>
+</div>
+"""
 
-st.markdown('### **1️⃣ Enter a URL to scrape **')
+def main():
+	"""A Simple CRUD  Blog"""
+	
+	st.markdown(html_temp.format('royalblue','white'),unsafe_allow_html=True)
 
-try:
+	menu = ["Home","View Posts","Add Posts","Search","Manage Blog"]
+	choice = st.sidebar.selectbox("Menu",menu)
 
-    url =  st.text_input("", value='https://stackexchange.com/leagues/1/alltime/stackoverflow', max_chars=None, key=None, type='default')
+	if choice == "Home":
+		st.subheader("Home")
+		result = view_all_notes()
+		
+		for i in result:
+			b_author = i[0]
+			b_title = i[1]
+			b_article = str(i[2])[0:30]
+			b_post_date = i[3]
+			st.markdown(title_temp.format(b_title,b_author,b_article,b_post_date),unsafe_allow_html=True)
 
-    if url:
-        
-        arr = ['https://', 'http://']
-        if any(c in url for c in arr):
-
-
-    #    if "https" in url:
-
-            header = {
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36",
-            "X-Requested-With": "XMLHttpRequest"
-            }
-
-            @st.cache(persist=True, show_spinner=False)
-            def load_data():
-                r = requests.get(url, headers=header)
-                return pd.read_html(r.text)
-
-            df = load_data()
-
-            length = len(df)
-            
-            if length == 1:
-                st.write("This webpage contains 1 table" )
-            else: st.write("This webpage contains " + str(length) + " tables" )
-
-            
-            #st.write("This webpage contains " + str(length) + " tables" )
-
-            if st.button("Show scraped tables"):
-                st.table(df)
-            else: st.empty()
-
-            def createList(r1, r2): 
-                return [item for item in range(r1, r2+1)] 
-                
-            r1, r2 = 1, length
-            funct = createList(r1, r2)
-
-            ###### Selectbox - Selectbox - Selectbox - Selectbox - Selectbox - Selectbox - Selectbox - 
-
-            st.markdown('### **2️⃣ Select a table to export **')
-            
-            ValueSelected = st.selectbox('', funct)
-            st.write('You selected table #', ValueSelected)
+	elif choice == "View Posts":
+		st.subheader("View Articles")
+		all_titles = [i[0] for i in view_all_titles()]
+		postlist = st.sidebar.selectbox("View Posts",all_titles)
+		post_result = get_blog_by_title(postlist)
+		for i in post_result:
+			b_author = i[0]
+			b_title = i[1]
+			b_article = i[2]
+			b_post_date = i[3]
+			st.text("Reading Time:{}".format(readingTime(b_article)))
+			st.markdown(head_message_temp.format(b_title,b_author,b_post_date),unsafe_allow_html=True)
+			st.markdown(full_message_temp.format(b_article),unsafe_allow_html=True)
 
 
-            df1 = df[ValueSelected -1]
+
+	elif choice == "Add Posts":
+		st.subheader("Add Articles")
+		create_table()
+		blog_author = st.text_input("Enter Author Name",max_chars=50)
+		blog_title = st.text_input("Enter Post Title")
+		blog_article = st.text_area("Post Article Here",height=200)
+		blog_post_date = st.date_input("Date")
+		if st.button("Add"):
+			add_data(blog_author,blog_title,blog_article,blog_post_date)
+			st.success("Post:{} saved".format(blog_title))	
 
 
-            if df1.empty:
 
-                st.warning ('ℹ️ - This DataFrame is empty!')
 
-            
-            else:
-                
-                #df1.index = df1.index.map(str)
-                df1 = df1.replace(np.nan, 'empty cell', regex=True)
-                #df1 = df1.replace('vte','')
-                #df1.columns = df1.columns.str.replace(r'vte','')
-                st.dataframe(df1)
-                
-            #df.columns = df.columns.str.replace(r"[()]", "_
-            #df2 = df1.val.replace({'vte':'test'}, regex=True)
-        else:
-            st.error ('⚠️ - URL needs to be in a valid format, starting with *https://* or *http://*')
-            
-    else:
-        
-        st.warning ('ℹ️ - Paste a URL in the field above')
-    
-except ValueError:
-    st.info ("ℹ️ - No table(s) to scrape on this page! 😊")
+	elif choice == "Search":
+		st.subheader("Search Articles")
+		search_term = st.text_input('Enter Search Term')
+		search_choice = st.radio("Field to Search By",("title","author"))
+		
+		if st.button("Search"):
 
-try:
-    
-    csv = df1.to_csv(index=False)
-    b64 = base64.b64encode(csv.encode()).decode()
-    st.markdown('### ** ⬇️ Download the selected table to CSV **')
-    href = f'<a href="data:file/csv;base64,{b64}" download="filtered_table.csv">** Click here to get your prize! 🎉**</a>'
-    st.markdown(href, unsafe_allow_html=True)
+			if search_choice == "title":
+				article_result = get_blog_by_title(search_term)
+			elif search_choice == "author":
+				article_result = get_blog_by_author(search_term)
 
-except NameError:
-    print ('wait')
 
-st.markdown("---")
+			for i in article_result:
+				b_author = i[0]
+				b_title = i[1]
+				b_article = i[2]
+				b_post_date = i[3]
+				st.text("Reading Time:{}".format(readingTime(b_article)))
+				st.markdown(head_message_temp.format(b_title,b_author,b_post_date),unsafe_allow_html=True)
+				st.markdown(full_message_temp.format(b_article),unsafe_allow_html=True)
 
-st.markdown('*Made with* :heart: * by [@DataChaz ](https://twitter.com/DataChaz)* [![this is an image link](https://i.imgur.com/Ltgzb7Y.png)](https://www.buymeacoffee.com/cwar05)')
+
+
+
+	elif choice == "Manage Blog":
+		st.subheader("Manage Articles")
+
+		result = view_all_notes()
+		clean_db = pd.DataFrame(result,columns=["Author","Title","Articles","Post Date"])
+		st.dataframe(clean_db)
+
+		unique_titles = [i[0] for i in view_all_titles()]
+		delete_blog_by_title = st.selectbox("Unique Title",unique_titles)
+		new_df = clean_db
+		if st.button("Delete"):
+			delete_data(delete_blog_by_title)
+			st.warning("Deleted: '{}'".format(delete_blog_by_title))
+
+
+		if st.checkbox("Metrics"):
+			
+			new_df['Length'] = new_df['Articles'].str.len()
+			st.dataframe(new_df)
+
+
+			st.subheader("Author Stats")
+			new_df["Author"].value_counts().plot(kind='bar')
+			st.pyplot()
+
+			st.subheader("Author Stats")
+			new_df['Author'].value_counts().plot.pie(autopct="%1.1f%%")
+			st.pyplot()
+
+		if st.checkbox("Word Cloud"):
+			st.subheader("Generate Word Cloud")
+			# text = new_df['Articles'].iloc[0]
+			text = ','.join(new_df['Articles'])
+			wordcloud = WordCloud().generate(text)
+			plt.imshow(wordcloud,interpolation='bilinear')
+			plt.axis("off")
+			st.pyplot()
+
+		if st.checkbox("BarH Plot"):
+			st.subheader("Length of Articles")
+			new_df = clean_db
+			new_df['Length'] = new_df['Articles'].str.len()
+			barh_plot = new_df.plot.barh(x='Author',y='Length',figsize=(20,10))
+			st.pyplot()
+
+
+if __name__ == '__main__':
+	main()
+
